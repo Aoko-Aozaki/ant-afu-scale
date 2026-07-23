3971a3e052a9199dd0377356153257075f86c075
Shuhui <shuhui@msn.com>
2026-07-23 15:23:31 -0400

    Initial Commit

---
+++ Scale App/Scale_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scale_AppApp.swift
+//  Scale App
+//
+//  Created by Francium on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Scale_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
